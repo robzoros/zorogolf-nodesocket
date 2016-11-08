2338e9df480c896657a12dd13c0775f7f32a9655
robzoros <robzoros@gmail.com>
2016-11-08 16:29:14 +0100

    Inicio Git

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
-var React = require('react'),
-    path = require('path'),
-    compression = require('compression'),
-    renderToString = require('react-dom/server').renderToString,
-    reactRouter = require('react-router'),
-    match = reactRouter.match,
-    RouterContext  = reactRouter.RouterContext,
-    routes = require('../public/app/api/routes.jsx'),
-    express= require('express');
+import React from 'react'
+import path from 'path'
+import compression from 'compression'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import routes from '../public/app/api/routes.jsx'
+import express from 'express'
 
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 
 var messages = [{
   id: 1,
@@ -18,28 +16,29 @@ var messages = [{
   author: "Carlos Azaustre"
 }];
 
-app.use(compression());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../public'));
+app.use(compression())
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../public'))
 
 // serve our static stuff like index.css
-app.use(express.static(path.join(__dirname, '../public'), {index: false}));
+app.use(express.static(path.join(__dirname, '../public'), {index: false}))
 
 app.get('*', (req, res) => {
     match({ routes, location: req.url }, (err, redirect, props) => {
+        console.log(req.url)
         if (err) {
             res.status(500).send(err.message)
         } else if (redirect) {
             res.redirect(redirect.pathname + redirect.search)
         } else if (props) {
             // hey we made it!
-            const appHtml = renderToString(`<RouterContext {...props}/>` )
+            const appHtml = renderToString(<RouterContext {...props}/>)
             return res.render('index', { markup: appHtml });
         } else {
             res.status(404).send('Not Found')
         }
     })
-});
+})
 
 io.on('connection', function(socket) {
     console.log(new Date() + ': Alguien se ha conectado con Sockets.');
@@ -55,4 +54,4 @@ io.on('connection', function(socket) {
 var PORT = process.env.PORT || 8080
 server.listen(PORT, function() {
     console.log('Production Express server running at localhost:' + PORT)
-});
+})
