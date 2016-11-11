e78c22707803dfbc165631db4abe1eec0047ad13
robzoros <robzoros@gmail.com>
2016-11-11 10:50:59 +0100

    Quitando Server Sider Rendering

--- server/server.js
+++ server/server.js
@@ -1,44 +1,24 @@
-import React from 'react'
-import path from 'path'
-import compression from 'compression'
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from '../src/api/routes.jsx'
-import express from 'express'
-
-var app = express()
-var server = require('http').Server(app)
-var io = require('socket.io')(server)
+var compression = require('compression'),
+    express = require('express'),
+    path = require('path'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
 
 var messages = [{
-  id: 1,
-  text: "Hola soy un mensaje",
-  author: "Carlos Azaustre"
+    id: 1,
+    text: "Hola soy un mensaje",
+    author: "Carlos Azaustre"
 }];
 
-app.use(compression())
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '../public'))
+app.use(compression());
 
 // serve our static stuff like index.css
-app.use(express.static(path.join(__dirname, '../public'), {index: false}))
+app.use(express.static(path.join(__dirname, '../public'), {index: false}));
 
 app.get('*', (req, res) => {
-    match({ routes, location: req.url }, (err, redirect, props) => {
-        console.log(req.url)
-        if (err) {
-            res.status(500).send(err.message)
-        } else if (redirect) {
-            res.redirect(redirect.pathname + redirect.search)
-        } else if (props) {
-            // hey we made it!
-            const appHtml = renderToString(<RouterContext {...props}/>)
-            return res.render('index', { markup: appHtml });
-        } else {
-            res.status(404).send('Not Found')
-        }
-    })
-})
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 io.on('connection', function(socket) {
     console.log(new Date() + ': Alguien se ha conectado con Sockets.');
@@ -51,7 +31,7 @@ io.on('connection', function(socket) {
     });
 });
 
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080;
 server.listen(PORT, function() {
-    console.log('Production Express server running at localhost:' + PORT)
-})
+    console.log('Production Express server running at localhost:' + PORT);
+});
